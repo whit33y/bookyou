10a1a97873f3c97afa2b8d8a0b46bc1784719e1a
Jakub Bryska <31563900+whit33y@users.noreply.github.com>
2026-05-11 21:09:16 +0200

    fix(backend): bug with prisma and backend (#14)

--- apps/backend/src/prisma/prisma.service.ts
+++ apps/backend/src/prisma/prisma.service.ts
@@ -1,16 +1,30 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { ConfigService } from '@nestjs/config';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  private pool: Pool;
+
+  constructor(config: ConfigService) {
+    const url = config.get<string>('DATABASE_URL');
+    const pool = new Pool({ connectionString: url });
+    const adapter = new PrismaPg(pool);
+    super({ adapter } as any);
+    this.pool = pool;
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
 
   async onModuleDestroy() {
     await this.$disconnect();
+    await this.pool.end();
   }
 }
