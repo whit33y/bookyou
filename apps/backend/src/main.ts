a646e2a6d6afd6aa63efc88ace6d6346e554b8e4
whit33y <troppout@gmail.com>
2026-05-12 20:43:00 +0200

    fix(api): cors error

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -17,6 +17,7 @@ async function bootstrap() {
     }),
   );
 
+  app.enableCors({ origin: 'http://localhost:4200', credentials: true });
   app.setGlobalPrefix('api');
 
   // Enable Graceful Shutdown for Prisma
